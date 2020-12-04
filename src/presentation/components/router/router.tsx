191862e904966ff3b5bdde4950ce0715eebdfa4b
Fernando Linhares <fernandolinhares1920@globo.com>
2020-12-04 18:40:54 -0300

    fix: move global styles to index

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// Css
-import '@/presentation/styles/global.scss'
 // Components
 import { Login } from '@/presentation/pages'
 
