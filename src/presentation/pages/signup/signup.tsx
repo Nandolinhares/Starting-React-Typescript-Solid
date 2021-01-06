72a6e4d22352eeca04aa153f08e886913a270ea8
Fernando Linhares <fernandolinhares1920@globo.com>
2021-01-06 17:10:49 -0300

    feat: add signup view

---
+++ src/presentation/pages/signup/signup.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Styles from './signup-styles.scss'
+// Components
+import { LoginHeader, Input, FormStatus, Footer } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
+
+const Signup: React.FC = () => {
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+      <Context.Provider value={ { state: {} }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
+          <Input type="text" name="name" placeholder="Digite seu nome" />
+          <Input type="email" name="email" placeholder="Digite seu email" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input type="password" name="passwordConfirmation" placeholder="Repit sua senha" />
+          <button className={Styles.submit} type="submit">Entrar</button>
+          <Link to="/login" className={Styles.link}>Voltar para login</Link>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  )
+}
+
+export default Signup
